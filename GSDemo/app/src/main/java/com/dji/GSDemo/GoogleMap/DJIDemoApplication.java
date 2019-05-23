84ecc136f11296832882ce527177f0cf5383fd08
OliverOu <oliver.ou@dji.com>
2019-05-23 21:59:43 +0800

    update GSDemo to Android SDK 4.10.

--- GSDemo/app/src/main/java/com/dji/GSDemo/GoogleMap/DJIDemoApplication.java
+++ GSDemo/app/src/main/java/com/dji/GSDemo/GoogleMap/DJIDemoApplication.java
@@ -13,6 +13,7 @@ import android.widget.Toast;
 
 import dji.sdk.base.BaseComponent;
 import dji.sdk.base.BaseProduct;
+import dji.sdk.sdkmanager.DJISDKInitEvent;
 import dji.sdk.sdkmanager.DJISDKManager;
 import dji.common.error.DJIError;
 import dji.common.error.DJISDKError;
@@ -122,6 +123,10 @@ public class DJIDemoApplication extends Application {
                                 componentKey,
                                 oldComponent,
                                 newComponent));
+
+            }
+            @Override
+            public void onInitProcess(DJISDKInitEvent djisdkInitEvent, int i) {
 
             }
         };

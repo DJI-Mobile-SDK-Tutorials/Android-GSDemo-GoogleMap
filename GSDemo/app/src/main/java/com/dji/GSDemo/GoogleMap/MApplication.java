f2fa25d5d5bc99ba557d6affd3877e8a138abef5
konglobemeralt <jesper.blidkvist@live.se>
2022-04-21 10:29:05 +0200

    Works now

--- GSDemo/app/src/main/java/com/dji/GSDemo/GoogleMap/MApplication.java
+++ GSDemo/app/src/main/java/com/dji/GSDemo/GoogleMap/MApplication.java
@@ -2,6 +2,7 @@ package com.dji.GSDemo.GoogleMap;
 
 import android.app.Application;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.util.Log;
 
 import com.secneo.sdk.Helper;
@@ -16,11 +17,9 @@ public class MApplication extends Application {
             Log.wtf("Error", e);
         }
 
-        Task droneTask = new Task();
-        droneTask.run();
 
-        IsoDrone drone = new IsoDrone("192.168.21.87");
-        Log.wtf("IPv4", Utils.getIPAddress(true)); // IPv4
+
+
     }
 
 
@@ -43,6 +42,9 @@ public class MApplication extends Application {
     public void onCreate() {
 
         super.onCreate();
+
+        Task droneTask = new Task();
+        droneTask.run();
         //fpvDemoApplication.onCreate();
 
     }
@@ -54,17 +56,46 @@ public class MApplication extends Application {
 class Task implements Runnable {
     @Override
     public void run() {
+        IsoDrone drone = new IsoDrone("192.168.21.87");
+    while(true) {
+       try {
+          Thread.sleep(100);
+          Log.wtf("Name", drone.getName());
+          Log.wtf("State", drone.getCurrentStateName());
+          Log.wtf("IPv4", Utils.getIPAddress(true)); // IPv4
 
-   // while(true) {
-   //    try {
-   //       //Thread.sleep(1000);
-   //       //Log.wtf("Name", drone.getName());
-   //       //Log.wtf("State", drone.getCurrentStateName());
-   //       //Log.wtf("IPv4", Utils.getIPAddress(true)); // IPv4
-//
-   //    } catch (InterruptedException e) {
-   //        e.printStackTrace();
-   //    }
-   // }
+       } catch (InterruptedException e) {
+           e.printStackTrace();
+       }
+    }
     }
 }
+
+/*
+class AsyncTaskRunner extends AsyncTask<String, String, String> {
+
+    @Override
+    protected String doInBackground(String... params) {
+        IsoDrone drone = new IsoDrone("192.168.21.87");
+        Log.wtf("IPv4", Utils.getIPAddress(true)); // IPv4
+        return "";
+
+    }
+
+
+    @Override
+    protected void onPostExecute(String result) {
+
+    }
+
+
+    @Override
+    protected void onPreExecute() {
+    }
+
+
+    @Override
+    protected void onProgressUpdate(String... text) {
+    }
+}
+*/
